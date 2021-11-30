6865e42bdb128fd91a3d81862074570162c4cde6
DongHyeon Kim <hyeoni369@naver.com>
2021-11-30 23:40:35 +0900

    검색바 다시 구현

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,42 @@
 import React, { useState } from "react";
-import { Input } from "antd";
 import { useNavigate } from "react-router-dom";
-import "antd/dist/antd.css";
+import { SearchOutlined } from "@ant-design/icons";
+import "../App.css";
 
 const SearchBar = () => {
   const [keyword, setKeyword] = useState("");
   const navigate = useNavigate();
 
-  const onSearch = () => {
-    // console.log(keyword);
+  const onChange = (e) => {
+    setKeyword(e?.target?.value);
+  };
+
+  const onSearch = (e) => {
+    e.preventDefault();
+
+    if (!keyword) {
+      alert("검색어를 입력해주세요.");
+      return;
+    }
+
     navigate(`/search?keyword=${keyword}`);
   };
 
   return (
-    <Input.Search
-      size="large"
-      onChange={(e) => setKeyword(e?.target?.value)}
-      placeholder="찾고 싶은 상품을 검색해보세요!"
-      onSearch={onSearch}
-      enterButton
-    />
+    <form className="search-bar" onSubmit={(e) => onSearch(e)}>
+      <input
+        className="search"
+        placeholder="찾고 싶은 상품을 검색해보세요!"
+        onChange={onChange}
+      />
+      <SearchOutlined
+        onClick={onSearch}
+        style={{
+          fontSize: 16,
+          color: "#1890ff",
+        }}
+      />
+    </form>
   );
 };
 
