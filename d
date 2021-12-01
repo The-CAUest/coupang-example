4d7614098754b90d20b268d4c4e942cd6c152427
nahyun902 <nahyun902@naver.com>
2021-12-02 04:17:03 +0900

    style 수정 끝

---
+++ d
@@ -0,0 +1,119 @@
+[33mcommit bc672081d506acbb5bb39f60e6d106fd8b587758[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Thu Dec 2 03:09:00 2021 +0900
+
+    fixed untracked files
+
+[33mcommit e86acbd35d81bb9297d7936b7c849f8a99c6bc30[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 23:31:10 2021 +0900
+
+    amplify 폴더 제외
+
+[33mcommit 5dfb621db8af4b92fe99ae73e14fa93748399e5f[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 23:30:04 2021 +0900
+
+    상품 하나 읽기
+
+[33mcommit f5aa57db59051d91e0fbfcfb7c9376e29e82311d[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 23:17:18 2021 +0900
+
+    검색 끝
+
+[33mcommit 8275080c40b8905f816c74fc7b741d3ca26a6f29[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 23:10:46 2021 +0900
+
+    마이쿠팡
+
+[33mcommit 0fa16d82c35ea22918e3d0b2c52fe8517ff35096[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 22:57:22 2021 +0900
+
+    카트 추가하고 헤더에서 이동 가능하도록 수정
+
+[33mcommit e35d5d362d87f165e00fdfa3a5637d99f08da563[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 22:40:49 2021 +0900
+
+    장바구니로 text 수정
+
+[33mcommit e7bfcd65115732716502f473c5323a9882dbfd13[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 22:28:18 2021 +0900
+
+    카테고리 불러와서 카테고리별 상품 나오도록 함
+
+[33mcommit 9959518ce6ce2694c122236d65caea9a284550b0[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 22:14:06 2021 +0900
+
+    카테고리 하나로 예시 제작 완료
+
+[33mcommit 324c1ca08313bb863dfc8624395ab6587dbf8bd1[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 19:55:00 2021 +0900
+
+    메인화면 상품 리스트
+
+[33mcommit 9769979896001ce0643ce38b5956455471de27a4[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 04:35:45 2021 +0900
+
+    탑 바 에 왼쪽 카테고리 파란 버튼 넣음
+
+[33mcommit b460cd72655e51eff5dc430ccbc4116c373d1341[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 04:24:49 2021 +0900
+
+    메인화면 이미지 슬라이더 넣음
+
+[33mcommit 46a4944a142f5b903d2c1fda61ad3a3364d4c873[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 02:04:34 2021 +0900
+
+    탑 바 로 묶음
+
+[33mcommit d7084d0ca8d06f355efde1a99303d28f272d94ad[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 01:55:36 2021 +0900
+
+    검색바 최종
+
+[33mcommit e1c9bb8f3088b9242ce93604f35d4211eea163e5[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 01:43:42 2021 +0900
+
+    검색바 수정
+
+[33mcommit 22d29da36318b22327bb2b5e4741569cd4a6faf3[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Wed Dec 1 00:15:52 2021 +0900
+
+    쿠팡 상단바 수정
+
+[33mcommit 6865e42bdb128fd91a3d81862074570162c4cde6[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Tue Nov 30 23:40:35 2021 +0900
+
+    검색바 다시 구현
+
+[33mcommit 4e4f99e374baa5bef2b2e9cdb416e8d17874af82[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Tue Nov 30 22:44:45 2021 +0900
+
+    쓸데없는 div 제거
+
+[33mcommit b9567c62e7564b9f04dfb4b2e537e506a8d8c822[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Tue Nov 30 22:43:57 2021 +0900
+
+    검색바 구현
+
+[33mcommit 395e47bd9c1677fc6e912ebedfc419a763ede9c1[m
+Author: DongHyeon Kim <hyeoni369@naver.com>
+Date:   Mon Nov 22 19:21:12 2021 +0900
+
+    Initialize project using Create React App
--- src/router/Cart.js
+++ src/router/Cart.js
@@ -1,39 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { ShoppingCartOutlined } from "@ant-design/icons";
-import { Button } from "antd";
-import "antd/dist/antd.css";
-import ProductList from "../components/crudl/ProductList";
-import Product from "../classes/crudl/Product";
+import React, { useEffect, useState } from 'react'
+import { ShoppingCartOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
+import 'antd/dist/antd.css'
+import ProductList from '../components/crudl/ProductList'
+import Product from '../classes/crudl/Product'
+import { useNavigate } from "react-router";
 
-function Cart() {
-  const [totalPrice, setPrice] = useState(0);
+function Cart () {
+  const [totalPrice, setPrice] = useState(0)
+  const navigate = useNavigate();
 
   useEffect(() => {
-    let total_price = 0;
+    let total_price = 0
     Product.listProduct().then((data) => {
       for (let i = 0; i < data.length; i++) {
-        total_price += data[i].price;
+        total_price += data[i].price
       }
-      setPrice(total_price);
-    });
-  }, []);
+      setPrice(total_price)
+    })
+  }, [])
 
   return (
-    <div style={{ backgroundColor: "#EBEBEB" }}>
+    <div style={{ backgroundColor: '#EBEBEB' }}>
       <header style={styles.header}>
         <a href="/">
           <img
             alt="coupang"
             style={styles.coupang}
             src={
-              "https://image7.coupangcdn.com/image/coupang/common/logo_coupang_w350.png"
+              'https://image7.coupangcdn.com/image/coupang/common/logo_coupang_w350.png'
             }
           />
         </a>
       </header>
       <section style={styles.contentsCart}>
         <section>
-          <ShoppingCartOutlined style={styles.cartIcon} />
+          <ShoppingCartOutlined style={styles.cartIcon}/>
           <h1 style={styles.h1}>장바구니</h1>
         </section>
 
@@ -42,10 +44,9 @@ function Cart() {
             <h4>전체 상품</h4>
             <h4>상품 정보</h4>
             <h4>상품 금액</h4>
-            <h4>배송비</h4>
           </div>
           <ProductList
-            showList={["img_imageUrl", "name", "price"]}
+            showList={['img_imageUrl', 'name', 'price']}
             style={styles.list}
           />
           <div class="cartTotalPrice" style={styles.cartTotalPrice}>
@@ -77,16 +78,17 @@ function Cart() {
               <h3 class="h" style={styles.h}>
                 총 주문금액
               </h3>
-              <strong style={{ color: "red", fontSize: "1.2em" }}>
+              <strong style={{ color: 'red', fontSize: '1.2em' }}>
                 {totalPrice}원
               </strong>
             </div>
           </div>
           <div
             class="order-buttons"
-            style={{ marginTop: "30px", textAlign: "center" }}
+            style={{ marginTop: '30px', textAlign: 'center' }}
           >
-            <Button id="button" style={styles.button} type="primary" ghost>
+            <Button id="button" style={styles.button} type="primary" onClick={() => navigate('/')}
+                    ghost>
               계속 쇼핑하기
             </Button>
             <Button id="button" style={styles.button} type="primary">
@@ -95,82 +97,82 @@ function Cart() {
           </div>
         </div>
       </section>
-      <footer style={{ textAlign: "center" }}>
+      <footer style={{ textAlign: 'center' }}>
         <h3>Wraplify</h3>
       </footer>
     </div>
-  );
+  )
 }
 
 const styles = {
   header: {
-    width: "1010px",
-    margin: "0 auto 10px",
+    width: '1010px',
+    margin: '0 auto 10px',
   },
   coupang: {
-    display: "block",
-    width: "140px",
+    display: 'block',
+    width: '140px',
     padding: '20px 0 10px',
   },
   contentsCart: {
-    width: "1010px",
-    border: "1px solid #e0e0e0",
-    margin: "10px auto 70px",
-    padding: "40px 39px",
-    backgroundColor: "#fff",
+    width: '1010px',
+    border: '1px solid #e0e0e0',
+    margin: '10px auto 70px',
+    padding: '40px 39px',
+    backgroundColor: '#fff',
   },
   cartTitle: {
-    height: "45px",
-    paddingBottom: "32px",
-    fontSize: "16px",
+    height: '45px',
+    paddingBottom: '32px',
+    fontSize: '16px',
   },
   cartIcon: {
-    float: "left",
+    float: 'left',
     marginRight: 10,
-    marginBottom: 30,
-    fontSize: "40px",
+    marginTop: 3,
+    fontSize: '40px',
   },
-  h1:{
-
+  h1: {
+    fontSize: 30
   },
   tableContent: {
-    fontSize: "14px",
-    verticalAlign: "middle",
+    fontSize: '14px',
+    verticalAlign: 'middle',
   },
   tableTop: {
-    display: "flex",
-    justifyContent: "space-between",
-    backgroundColor: "#EBEBEB",
-    marginTop: 30,
-    padding: "5px 20px 0px 20px",
+    display: 'flex',
+    justifyContent: 'space-between',
+    backgroundColor: '#EBEBEB',
+    marginTop: 20,
+    padding: '5px 20px 0px 20px',
   },
   cartTotalPrice: {
-    border: "4px solid #c8c8c8",
-    padding: "20px 15px 18px",
+    border: '4px solid #c8c8c8',
+    padding: '20px 15px 18px',
   },
   text: {
-    display: "flex",
-    justifyContent: "center",
+    display: 'flex',
+    justifyContent: 'center',
     fontSize: 14,
   },
   h: {
-    marginRight: "10px",
+    marginRight: '10px',
   },
   finalPrice: {
-    marginRight: "10px",
-    fontSize: "1.2em",
+    marginRight: '10px',
+    fontSize: '1.2em',
   },
   button: {
-    width: "140px",
-    height: "55px",
-    textAlign: "center",
-    verticalAlign: "middle",
-    margin: "0px 30px 0px 0px",
+    width: '140px',
+    height: '55px',
+    textAlign: 'center',
+    verticalAlign: 'middle',
+    margin: '0px 30px 0px 0px',
   },
   list: {
-    width: "100%",
-    marginBottom: "30px",
+    width: '100%',
+    marginBottom: '30px',
   },
-};
+}
 
-export default Cart;
+export default Cart
