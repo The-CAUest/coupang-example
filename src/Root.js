f5aa57db59051d91e0fbfcfb7c9376e29e82311d
DongHyeon Kim <hyeoni369@naver.com>
2021-12-01 23:17:18 +0900

    검색 끝

--- src/Root.js
+++ src/Root.js
@@ -5,6 +5,7 @@ import config from "./aws-exports";
 import Main from "./router/Main";
 import Cart from "./router/Cart";
 import MyCoupang from "./router/MyCoupang";
+import ProductSearchList from "./router/ProductSearchList";
 Amplify.configure(config);
 
 const Root = () => (
@@ -13,6 +14,7 @@ const Root = () => (
       <Route path="/" element={<Main />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/my-coupang" element={<MyCoupang />} />
+      <Route path="/search" element={<ProductSearchList />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ src/router/ProductSearchList.js
@@ -0,0 +1,66 @@
+import React from "react";
+import SearchProductList from "../components/SearchProductList";
+import TopBar from "../components/TopBar";
+
+function ProductSearchList() {
+  const onClick = () => {
+    console.log("click");
+  };
+
+  return (
+    <div
+      class="mycoupang"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <TopBar />
+      <div style={styles.searchHeader}>
+        전체 ><b>'여기 수정하기'</b>
+      </div>
+      <div style={styles.container}>
+        <div style={{ display: "flex" }}>
+          <b style={styles.borderText}> '여기 수정하기' </b>
+          <h4 style={styles.text}>에 대한 검색 결과</h4>
+        </div>
+        <SearchProductList
+          showList={["img_imgUrl", "name", "price"]}
+          onClick={onClick}
+          style={styles.listcomp}
+        />
+      </div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    width: "1100px",
+    margin: "1px auto 70px",
+    padding: "20px 20px",
+  },
+  text: {
+    marginBottom: "15px",
+  },
+  borderText: {
+    marginRight: "5px",
+  },
+  searchHeader: {
+    position: "relative",
+    height: "35px",
+    width: "100%",
+    border: "1px solid #eee",
+    backgroundColor: "#fafafa",
+    fontSize: "14px",
+    padding: "5px 100px",
+  },
+  listcomp: {
+    display: "flex",
+    justifyContent: "center",
+  },
+};
+
+export default ProductSearchList;
