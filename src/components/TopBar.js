46f774faa5d4717cf7f975d0198c5f5ce535c3b0
DongHyeon Kim <hyeoni369@naver.com>
2021-12-03 03:15:14 +0900

    상단바에 로그인하면 이름 나오도록 수정

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
 import {
   UserOutlined,
@@ -6,16 +6,39 @@ import {
   MenuOutlined,
 } from "@ant-design/icons";
 import { useNavigate } from "react-router";
+import WraplifyAuth from "../classes/auth/WraplifyAuth";
 
 const TopBar = ({ style }) => {
   const navigate = useNavigate();
+  const [user, setUser] = useState();
+  useEffect(() => WraplifyAuth.currentUser().then((u) => setUser(u)), []);
 
   return (
     <div className="header">
       <article>
         <div className="header-nav">
-          <span onClick={() => navigate("/login")}>로그인</span>
-          <span onClick={() => navigate("/login")}>회원가입</span>
+          {user ? (
+            <>
+              <span
+                onClick={() => navigate("/my-coupang")}
+                style={{ fontWeight: "bold", fontSize: 13 }}
+              >
+                {user?.attributes?.name}님
+              </span>
+              <span
+                onClick={() => {
+                  WraplifyAuth.logout().then(() => navigate("/"));
+                }}
+              >
+                로그아웃
+              </span>
+            </>
+          ) : (
+            <>
+              <span onClick={() => navigate("/login")}>로그인</span>
+              <span onClick={() => navigate("/login")}>회원가입</span>
+            </>
+          )}
           <span onClick={() => navigate("/login")}>고객센터</span>
         </div>
       </article>
